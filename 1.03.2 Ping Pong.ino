6af176c7c16a3c75509079ec75d3b36a92f2a118
water <89411131+skwuiiid@users.noreply.github.com>
2021-11-07 20:02:52 -0500

    Create 1.03.2 Ping Pong.ino

---
+++ 1.03.2 Ping Pong.ino
@@ -0,0 +1,37 @@
+void setup()
+{
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+
+}
+
+void loop()
+{
+  digitalWrite(4, HIGH);
+  delay (100);       
+  digitalWrite(5, HIGH);   
+  delay (100); 
+  digitalWrite(6, HIGH);    
+  delay (100);    
+  digitalWrite(7, HIGH);  
+  delay (100);
+  digitalWrite(8, HIGH);     
+
+
+  delay(200);
+  
+  digitalWrite(8, LOW);   
+  delay (100);    
+  digitalWrite(7, LOW);     
+  delay (100);
+  digitalWrite(6, LOW);    
+  delay (100);   
+  digitalWrite(5, LOW);     
+  delay (100);
+  digitalWrite(4, LOW);   
+  
+  delay(200);
+}
