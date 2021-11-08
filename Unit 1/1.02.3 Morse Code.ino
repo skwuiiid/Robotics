370b64996ee3d51135c5d67320aa033e7a6ff2c7
water <89411131+skwuiiid@users.noreply.github.com>
2021-11-08 18:15:28 -0500

    Update and rename 1.02.3 Morse Code.ino to Unit 1/1.02.3 Morse Code.ino

--- Unit 1/1.02.3 Morse Code.ino
+++ Unit 1/1.02.3 Morse Code.ino
@@ -1,3 +1,9 @@
+// Unit 1 Digital Sandbox
+// Tutorial 1.02 Lights On! Lights Off! (Blink LED)
+
+// Modify and repeat the blink sequence (HIGH → delay → LOW → delay) to spell out S-O-S, or a single word (no phrases/sentences).
+// Hint: Try working on one letter at a time!
+
 void setup()
 {
   pinMode(13, OUTPUT);
@@ -5,7 +11,6 @@ void setup()
 
 void loop()
 {
-
   delay(1000);    
 
   digitalWrite(13, HIGH);
